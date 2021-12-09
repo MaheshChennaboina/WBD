b19f98b9af8fa60351a1e1ad70e625fc4f2085db
daradeepak <deepak.dara07@gmail.com>
2021-12-09 13:49:41 +0530

    Done inserting images

--- ecommerce/src/components/Header.js
+++ ecommerce/src/components/Header.js
@@ -6,12 +6,12 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
   <Container>
-    <Navbar.Brand href="/">E-Commerce</Navbar.Brand>
+    <Navbar.Brand href="/"> NATURES BASKET </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ms-auto">
-        <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-        <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+        <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> CART </Nav.Link>
+        <Nav.Link href="/login"><i className='fas fa-user'></i> SIGN IN </Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
