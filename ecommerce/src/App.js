5f1381b2c7f9f9fb6ba533c8525deeb5f96362b7
daradeepak <deepak.dara07@gmail.com>
2021-12-15 12:55:36 +0530

    CartScreen inprogress

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+//import LoginScreen from "./screens/LoginScreen";
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from "./screens/CartScreen";
 
