92ea513bfe314b1e4ecd3ef0c05a94e717f6c79a
daradeepak <deepak.dara07@gmail.com>
2021-12-19 15:17:42 +0530

    Done

--- ecommerce/src/screens/HomeScreen.js
+++ ecommerce/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreen = () => {
             <h1> Latest Products for You </h1>
             <Row>
                 {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Col key={product._id} sm={12} md={6} lg={5} xl={3}>
                     <Product product={product} />
                     </Col>
                 ))}
