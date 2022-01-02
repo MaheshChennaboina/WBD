4e04fda7350d986b036091494b7c289f68e5d261
daradeepak <deepak.dara07@gmail.com>
2022-01-02 15:44:33 +0530

    .

--- ecommerce/src/screens/LoginScreen.js
+++ ecommerce/src/screens/LoginScreen.js
@@ -1,25 +1,131 @@
-import React from 'react'
-import { BrowserRouter as Router , Route,Link, Switch } from "react-router-dom";
-import loop from './loginscreen.module.css'
+// import React from 'react'
+// import { BrowserRouter as Router , Route,Link, Switch } from "react-router-dom";
+// import loop from './loginscreen.module.css'
 
 
-const LoginScreen = ()=>{
-    return(
-         <div className={loop.LoginScreen}>
-            <h2 className={loop.title}>Sign In</h2>
-           <form className={loop.form}>
-              <label>Email Address</label>
-              <input type='email' placeholder='Enter email'></input>
-              <label>password</label>
-              <input type='password' placeholder='password'></input>
-              <button type='submit' className={loop.btn1}>Sign In</button>
-             </form>
-             <Route>
-            <h3>New Customer ?<Link to="/signup">Register</Link></h3> 
-            </Route>
+// const LoginScreen = ()=>{
+//     return(
+//          <div className={loop.LoginScreen}>
+//             <h2 className={loop.title}>Sign In</h2>
+//            <form className={loop.form}>
+//               <label>Email Address</label>
+//               <input type='email' placeholder='Enter email'></input>
+//               <label>password</label>
+//               <input type='password' placeholder='password'></input>
+//               <button type='submit' className={loop.btn1}>Sign In</button>
+//              </form>
+//              <Route>
+//             <h3>New Customer ?<Link to="/signup">Register</Link></h3> 
+//             </Route>
          
-         </div>
+//          </div>
          
-)
+// )
+// }
+// export default LoginScreen;
+
+
+import axios from "axios";
+import React, { useState, useRef } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom"; // eslint-disable-line
+import { useHistory } from "react-router-dom";
+import '../CSS/login.css'
+
+export default function Login() {
+  const [username1, setUsername] = useState(" ");
+  const [password1, setPassword] = useState(" ");
+  const history = useHistory();
+  const incorrectCredentials = useRef(null);
+  
+
+  const LoginUser = () => {
+    //Hitting the Login api using axios by sending the useremail and password to authenticate
+    axios
+      .post("/login", { username: username1, password: password1 })
+      .then((result) => {
+        console.log("Successfully Logged In\n", result);
+        incorrectCredentials.current.innerText = " ";
+        history.push("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        incorrectCredentials.current.innerText =
+          "Incorrect Login Credentials.. Please try again";
+      });
+  };
+  // Show password feature
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
+  return (
+    
+    <div className="login">
+      <br />
+      <br />
+      <br />
+      <br />
+      <p
+        ref={incorrectCredentials}
+        style={{ textAlign: "center", color: "red" }}
+      ></p>
+      <div className="Container">
+        <br />
+        <h1>SIGN IN</h1>
+        <label className="Label" htmlFor="useremail">
+          <b>Email Addresss</b>
+        </label>
+        <br />
+        <input
+          className={["credentials", "login-input"].join(" ")}
+          id="useremail"
+          name="useremail"
+          type="text"
+          placeholder="Enter Email"
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <br />
+        <label className="Label" htmlFor="password">
+          <b>Password</b>
+        </label>
+        <br />
+        <input
+          // this is used to give multiple classnames to a component
+          className={["credentials", "login-input"].join(" ")}
+          placeholder="Enter Password"
+          id="password"
+          name="password"
+          type={passwordShown ? "text" : "password"}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br />
+        <div id="show-password">
+          <input
+            id="checkbox"
+            type="checkbox"
+            onClick={togglePasswordVisiblity}
+          />
+          <b> Show Password</b>
+          <br />
+        </div>
+        <button className="submit" type="submit" onClick={LoginUser}>
+          Submit
+          <br />
+        </button>
+        <br />
+        <br />
+        <br />
+        <div className="signup">
+          <b>New Customer ? </b>
+          <b>
+            <Link to="/signup">Register</Link>
+          </b>
+        </div>
+        <br />
+        <br />
+      </div>
+    </div>
+  );
 }
-export default LoginScreen;
--- ecommerce/src/screens/SignUP.js
+++ ecommerce/src/screens/SignUP.js
@@ -1,71 +1,242 @@
-import React from 'react'
-import {useState} from "react"
-import {Link} from "react-router-dom";
-import styles from './signup.module.css'
-import axios from 'axios'
+// import React from 'react'
+// import {useState} from "react"
+// import {Link} from "react-router-dom";
+// import styles from './signup.module.css'
+// import axios from 'axios'
 
 
-const SignUp = ()=>{
-    const [name ,setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("")
-    const [conformPassword,setConformPassword]= useState("")
-    const [message,setMessage] = useState("")
-     const nameHandler = (event)=>{
-        event.preventDefault();
-        setName(event.target.value)
-    }
-    const emailHandler = (event)=>{
-        event.preventDefault();
-        setEmail(event.target.value)
-    }
-    const passwordHandler = (event)=>{
-        event.preventDefault();
-        setPassword(event.target.value)
-    }
-    const conformPasswordHandler = (event)=>{
-        event.preventDefault();
-        setConformPassword(event.target.value)
-    }
+// const SignUp = ()=>{
+//     const [name ,setName] = useState("");
+//     const [email,setEmail] = useState("");
+//     const [password,setPassword] = useState("")
+//     const [conformPassword,setConformPassword]= useState("")
+//     const [message,setMessage] = useState("")
+//      const nameHandler = (event)=>{
+//         event.preventDefault();
+//         setName(event.target.value)
+//     }
+//     const emailHandler = (event)=>{
+//         event.preventDefault();
+//         setEmail(event.target.value)
+//     }
+//     const passwordHandler = (event)=>{
+//         event.preventDefault();
+//         setPassword(event.target.value)
+//     }
+//     const conformPasswordHandler = (event)=>{
+//         event.preventDefault();
+//         setConformPassword(event.target.value)
+//     }
 
-    const formHandler = (event)=>{
-        event.preventDefault();
-        const obj = {
-            "id":Math.random().toString(),
-            "name": name,
-            "Email": email,
-            "password": password,
-            "conformPassword": conformPassword
-        }
-        console.log(obj)
-        const url = "http://localhost:3002/user_details";
-        axios.post(url,obj)
-        .then((response)=>{
-          console.log(response.status)
-          setMessage("sucessssfully Register!")
+//     const formHandler = (event)=>{
+//         event.preventDefault();
+//         const obj = {
+//             "id":Math.random().toString(),
+//             "name": name,
+//             "Email": email,
+//             "password": password,
+//             "conformPassword": conformPassword
+//         }
+//         console.log(obj)
+//         const url = "http://localhost:3002/user_details";
+//         axios.post(url,obj)
+//         .then((response)=>{
+//           console.log(response.status)
+//           setMessage("sucessssfully Register!")
+//         })
+//        setName("")
+//        setPassword("");
+//        setConformPassword("")
+//        setEmail("")
+//     }
+
+//         return(
+//             <div className={styles.signup}>
+//                 <h1>Sign Up</h1>
+//                 <form className={styles.inside_form} onSubmit={formHandler}>
+//                     <label>Name</label>
+//                     <input type='text'name='name' placeholder="Enter name" onChange={nameHandler}/>
+//                     <label>Email Address</label>
+//                     <input tyoe='email' name='email' placeholder="Enter email" onChange={emailHandler}/>
+//                     <label>PassWord</label>
+//                     <input type='password' placeholder="Enter password"onChange={passwordHandler}/>
+//                     <label>Confirm Password</label>
+//                     <input type='password' placeholder="Confirm Password" onChange={conformPasswordHandler}/>
+//                 </form>
+//                 {message}
+//                 <h3 className="bottom">Have an Account?<Link to="/login" className="link">login</Link></h3>
+//             </div>
+//         );
+// }
+// export default SignUp 
+
+import React, { useState, useRef } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom"; // eslint-disable-line
+import { useHistory } from "react-router-dom";
+import "../CSS/signup.css"
+
+function Register() {
+  const history = useHistory();
+  //Declaring React Hooks to store the user data and post it to the JSON-Server.
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [useremail, setUseremail] = useState("");
+  const [mobile_number, setMobilenumber] = useState("");
+  const [userPassword, setPassword] = useState("");
+  // Hooks created for Show Password feature.
+  const [passwordShown, setPasswordShown] = useState(false);
+  const isUser = useRef(null);
+  // Show password function to toggle the password type from text to password.
+  const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
+  // Function declared to post the users details to the JSON-Server.
+  const registerUser = (event) => {
+    //Fetching the JSON-Server
+    fetch(`http://localhost:3000/users?q=${useremail}`)
+      .then((res) => res.json())
+      .then((json) => json[0].email)
+      .then((email) => {
+        //Checking whether User Already Exists
+        isUser.current.innerText =
+          "Email already in use. Please try again using another email id";
+      })
+      .catch((err) => {
+        isUser.current.innerText = " ";
+        const data = {
+          fulllName: firstname + " " + lastname,
+          email: useremail,
+          mobileNumber: mobile_number,
+          password: userPassword,
+          address: " ",
+          wishlist: [],
+          purchases: [],
+          cartItems: [],
+          reviews: [],
+          isLoggedIn: true,
+        };
+        fetch("http://localhost:3000/users", {
+          method: "POST", // or 'PUT'
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
         })
-       setName("")
-       setPassword("");
-       setConformPassword("")
-       setEmail("")
-    }
-
-        return(
-            <div className={styles.signup}>
-                <h1>Sign Up</h1>
-                <form className={styles.inside_form} onSubmit={formHandler}>
-                    <label>Name</label>
-                    <input type='text'name='name' placeholder="Enter name" onChange={nameHandler}/>
-                    <label>Email Address</label>
-                    <input tyoe='email' name='email' placeholder="Enter email" onChange={emailHandler}/>
-                    <label>PassWord</label>
-                    <input type='password' placeholder="Enter password"onChange={passwordHandler}/>
-                    <label>Confirm Password</label>
-                    <input type='password' placeholder="Confirm Password" onChange={conformPasswordHandler}/>
-                </form>
-                {message}
-                <h3 className="bottom">Have an Account?<Link to="/login" className="link">login</Link></h3>
-            </div>
-        );
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Success:", data);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+        event.preventDefault();
+        history.push("/");
+      });
+  };
+  return (
+    <React.Fragment>
+      <div className="signup-form">
+        <div className="form-container">
+          <p ref={isUser} style={{ textAlign: "center", color: "red" }}></p>
+          <br />
+          <h1 style={{ textAlign: "center" }}>Sign Up</h1>
+          <div>
+            <label className="Label" htmlFor="fname">
+              <h1>Your Name</h1>
+            </label>
+            <br />
+            <input
+              className="input-field width-43"
+              type="text"
+              name="fname"
+              id="fname"
+              placeholder="First Name"
+              onChange={(e) => setFirstname(e.target.value)}
+              required
+            />
+            <input
+              className="input-field width-43"
+              type="text"
+              name="lname"
+              id="lname"
+              placeholder="Last Name"
+              onChange={(e) => setLastname(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className="Label" htmlFor="email_id">
+              <h1>Email Id</h1>
+            </label>
+            {/* <br /> */}
+            <input
+              className="input-field width-90"
+              type="text"
+              name="email_id"
+              id="email_id"
+              placeholder="example@gmail.com"
+              onChange={(e) => setUseremail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className="Label" htmlFor="mobile_number">
+              <h3>Mobile Number</h3>
+            </label>
+            {/* <br /> */}
+            <input
+              className="input-field width-90"
+              type="text"
+              name="mobile_number"
+              id="mobile_number"
+              placeholder="999999999"
+              onChange={(e) => setMobilenumber(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label className="Label" htmlFor="password">
+              <h3>Password</h3>
+            </label>
+            <input
+              className="input-field width-90"
+              placeholder="Password"
+              id="password"
+              name="password"
+              type={passwordShown ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <br />
+          </div>
+          <div id="showpassword">
+            <input
+              id="checkbox"
+              type="checkbox"
+              onClick={togglePasswordVisiblity}
+            />
+            <b> Show Password</b>
+            <br />
+          </div>
+          <button
+            type="submit"
+            className="submit-register"
+            onClick={registerUser}
+          >
+            Register
+          </button>
+          <br />
+          <br />
+          <br />
+          <p style={{ textAlign: "center" }}>
+            <b>Existing User ? </b>
+            <Link to="/login">Sign in</Link>
+          </p>
+          <br />
+          <br />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
-export default SignUp 
+export default Register;
