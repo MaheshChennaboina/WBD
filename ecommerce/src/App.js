7b5240541ad1e0b69c8118afe5e3af846bec2059
daradeepak <deepak.dara07@gmail.com>
2022-01-08 20:37:30 +0530

    Comitting

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -6,8 +6,12 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import LoginScreen from "./screens/LoginScreen";
 import ProductScreen from './screens/ProductScreen'
-import SignUp from "./screens/SignUp";
 import CartScreen from "./screens/CartScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import OrderScreen from "./screens/OrderScreen";
 
 const App = () => {
   return (
@@ -16,10 +20,14 @@ const App = () => {
     <main>
       <Container>
         <Route path='/' component={HomeScreen} exact />
+        <Route path='/profile' component={ProfileScreen} />
         <Route path='/product/:id' component={ProductScreen} />
-        <Route path='/cart/:id?' component={CartScreen}></Route>
+        <Route path='/cart/:id?' component={CartScreen}/>
         <Route path ='/login' component={LoginScreen} />
-        <Route path='/signup' component={SignUp}/>
+        <Route path='/register' component={RegisterScreen}/>
+        <Route path='/shipping' component={ShippingScreen}/>
+        <Route path='/payment' component={PaymentScreen}/>
+        <Route path='/order/:id' component={OrderScreen}/>
 
       </Container>
     </main>
